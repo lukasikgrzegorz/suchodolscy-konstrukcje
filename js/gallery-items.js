8e6789098beff39e163ae5fd6858028772e77aae
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-10-07 08:27:15 +0200

    fix photo src

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,65 +1,65 @@
 export const galleryItems = [
   {
     preview:
-      './../images/photo_1_min.jpg',
+      './images/photo_1_min.jpg',
     original:
-      './../images/photo_1.jpg',
+      './images/photo_1.jpg',
     description: 'Konstrukcja',
   },
   {
    preview:
-      './../images/photo_2_min.jpg',
+      './images/photo_2_min.jpg',
     original:
-      './../images/photo_2.jpg',
+      './images/photo_2.jpg',
     description: 'Hala stalowa',
   },
   {
     preview:
-      './../images/photo_3_min.jpg',
+      './images/photo_3_min.jpg',
     original:
-      './../images/photo_3.jpg',
+      './images/photo_3.jpg',
     description: 'Hala stalowa',
   },
   {
    preview:
-      './../images/photo_4_min.jpg',
+      './images/photo_4_min.jpg',
     original:
-      './../images/photo_4.jpg',
+      './images/photo_4.jpg',
     description: 'Hala stalowa',
   },
   {
    preview:
-      './../images/photo_5_min.jpg',
+      './images/photo_5_min.jpg',
     original:
-      './../images/photo_5.jpg',
+      './images/photo_5.jpg',
     description: 'Hala stalowa',
   },
   {
    preview:
-      './../images/photo_6_min.jpg',
+      './images/photo_6_min.jpg',
     original:
-      './../images/photo_6.jpg',
+      './images/photo_6.jpg',
     description: 'Hala stalowa',
   },
   {
     preview:
-      './../images/photo_7_min.jpg',
+      './images/photo_7_min.jpg',
     original:
-      './../images/photo_7.jpg',
+      './images/photo_7.jpg',
     description: 'Hala stalowa',
   },
   {
     preview:
-      './../images/photo_8_min.jpg',
+      './images/photo_8_min.jpg',
     original:
-      './../images/photo_8.jpg',
+      './images/photo_8.jpg',
     description: 'Konstrukcja',
   },
   {
     preview:
-      './../images/photo_9_min.jpg',
+      './images/photo_9_min.jpg',
     original:
-      './../images/photo_9.jpg',
+      './images/photo_9.jpg',
     description: 'Garaż',
   },
 ];
